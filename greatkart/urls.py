f1a3cb36ce5345f1bdf1b501d6bd21cae1d7053e
pravinraj0001 <pravinraj0001@gmail.com>
2021-07-06 15:05:42 +0530

    server name

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -1,7 +1,7 @@
 """greatkart URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from greatkart.views import home
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path('admin/', include('admin_honeypot.urls',namespace='admin_honeypot')),
+    path('odysses/', admin.site.urls),
+    path('',home, name='home'),
+    path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
+    path('accounts/', include('accounts.urls')),
+    # ORDERS
+    path('orders/', include('orders.urls')),
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
